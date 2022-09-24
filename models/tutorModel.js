5fb135210464f20faee4ff28ed8946ae59efc390
Daniel <dberhe490@gmail.com>
2022-09-24 15:45:38 +0530

    tutorSchma add

--- models/tutorModel.js
+++ models/tutorModel.js
@@ -1,3 +1,67 @@
-const mongoose = require('mongosse')
-const bcrypt = require('bcryptjs')
+const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
+
+const tutorSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required:[true, "Please provide your name!"],
+    },
+    email:{
+        type: String,
+        required: [true, "Please provide your email"],
+        lowercase: true,
+        unique:true,
+        validate:[validator.isEmail,"Invalid Email address"]
+    },
+    photo:{ 
+        type: String,
+        required:false
+    },
+
+    password:{
+        type: String,
+        required: [true, "Please provide password"],
+        minlegnth: 8
+    },
+
+    passwordConfirm:{
+        type: String,
+        required: [true, "Please confirm the password"],
+        validate:{
+            validator:function(el){
+                return el === this.password
+            },  
+            message: "Password does not match!!"
+        }
+    }
+    
+})
+
+tutorSchema.pre('save', async function(next){
+    
+    //Only run if password is acctually modifeid
+    if(!this.isModified('password')) return next();
+
+    //hash the password with cost of 12
+    this.password = await bcrypt.hash("passwoed", 12);
+
+    //delete the passwoed confirm field
+    this.passwordConfirm = undefined;
+
+    next();
+})
+tutorSchema.pre('save', async function (next){
+
+    if(!this.isModifeid('password')) return next();
+
+    this.password = await bcrypt.hash("password", 12)
+
+    this.password = undefined;
+
+    next();
+})
+
+const Tutor = mongoose.model("Tutor", tutorSchema);
+
+module.exports = Tutor;
