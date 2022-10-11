c213780fe81fddb39b288d227693fad5d3c448cd
Daniel <dberhe490@gmail.com>
2022-10-11 11:47:00 +0530

    we have the forgot password endpoint api

--- controllers/authController.js
+++ controllers/authController.js
@@ -128,16 +128,34 @@ exports.restrictTo = (...roles) => {
 }
 exports.forgetPassword = catchAsync(async (req, res, next) => {
     //1) check if the POSTed email is existed
-    const student = await Student.findOne({ email: req.body.email })
-    if (!student) {
+    const user = await Student.findOne({ email: req.body.email })
+    if (!user) {
         return next(new AppError("There is no user with that email Addreess", 404))
     }
     //2) Generate the random rest token
-    const restToken = student.createPasswordResetToken();
-    await student.save({ validateBeforeSave: false });
+    const resetToken = user.createPasswordResetToken();
 
     //3) send it to the user
-
+   const resetURL = `${req.protocol}://${req.get('host')}/api/v1/students/resetPassword/${resetToken}`
+   const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you did not forgot your password, please ignore this email`;
+try{
+    await sendEmail({
+        email: user.email,
+        subject: "Your password reset token(valid for 10 min)",
+        message
+       });
+       res.status(200).json({
+        status:'success',
+        message: 'Token sent to email!'
+       });
+}catch(err){
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+    
+    return next(new AppError('There was an error sending the email. Try again later!', 500))
+}
+   
 })
 exports.restPassword = (req, res, next) => { }
 
