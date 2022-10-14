9c176019127111f6ea82fc204b7953e76006aa85
Daniel <dberhe490@gmail.com>
2022-10-14 22:45:27 +0530

    new update

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,12 @@
 import "@babel/polyfill"
-import {login, logout} from './login'
+import {login, logout, logint} from './login'
 
 document.querySelector('.form').addEventListener('submit', e =>{
     e.preventDefault();
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     login(email, password)
+    logint(email, password)
 })
 
 const logOutBtn = document.querySelector('.nav__el--logout')
